105e4829264f56444396b54fa8de19fd462ce37d
xinyang20--XM <15850645317@163.com>
2024-10-14 20:12:08 +0800

    上传241014全部代码

---
+++ 241014/Code/3.py
@@ -0,0 +1,28 @@
+import networkx as nx
+import matplotlib.pyplot as plt
+import numpy as np
+import re
+
+G=nx.Graph()
+node_list=[]
+lnum=0
+with open(r"../第三次实验材料/Jazz.txt","r") as file:
+    while True:
+        lines=file.readline()
+        if not lines:
+            break
+        lnum+=1
+        if lnum>=4:
+            temp=' '.join(re.split(' +|\n+',lines)).strip()
+            line=re.split(' ',temp.strip())
+            first=line[0]
+            second=line[1]
+            node_list.append(np.append(first,second))
+
+print(len(node_list))
+
+for i in range(len(node_list)):
+    G.add_edge(node_list[i][0],node_list[i][1])
+
+nx.draw(G,node_color='r',node_size=50)
+plt.show()
